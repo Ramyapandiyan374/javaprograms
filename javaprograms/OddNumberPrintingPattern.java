fa1c76fd244a8c9a8f516cb6614194a5c0fec925
Ramyapandiyan374 <146973975+Ramyapandiyan374@users.noreply.github.com>
2026-06-16 12:46:24 +0530

    Add files via upload

---
+++ javaprograms/OddNumberPrintingPattern.java
@@ -0,0 +1,31 @@
+package Dora;
+
+public class OddNumberPrintingPattern {
+public static void main(String[] args) {
+//	int n=3;
+//    int odd=1;
+//    for(int i=1;i<=n;i++) {
+//    	for(int j=1;j<=i;j++) {
+//    		System.out.print(odd+" ");
+//    		odd+=2;
+//    	}
+//    	System.out.println();
+//    }
+	
+	//printing normal numbers
+    int n=4;
+    int normal=1;
+    for(int i=1;i<=n;i++) {
+    	for(int j=1;j<=i;j++) {
+    		System.out.print(normal+" ");
+    		normal+=1;
+    	}
+    	System.out.println();
+    }
+    
+
+	
+	
+	
+}
+}
