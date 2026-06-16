a5d2158a4c58838f27a2231f1b9f6090165c71ac
Ramyapandiyan374 <146973975+Ramyapandiyan374@users.noreply.github.com>
2026-06-16 12:34:48 +0530

    Add files via upload

---
+++ NumberPatterns.java
@@ -0,0 +1,69 @@
+package Dora;
+
+public class NumberPatterns {
+public static void main(String[] args) {
+	int n=5;
+	//Left triangle with number
+//	for(int i=1;i<=n;i++) {
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	//Right triangle
+//	for(int i=1;i<=n;i++) {
+//		for(int j=1;j<=n-i;j++) {
+//			System.out.print("  ");
+//		}
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	
+	
+//	//upper triangle
+//	for(int i=n;i>=1;i--) {
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	//Lower triangle
+//	for(int i=n;i>=1;i--) {
+//		for(int j=1;j<=n-i;j++) {
+//			System.out.print("  ");
+//		}
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	//pyramid triangle
+//	for(int i=1;i<=n;i++) {
+//		for(int j=1;j<=n-i;j++) {
+//			System.out.print(" ");
+//		}
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	//Printing same number
+	
+//	for(int i=1;i<=n;i++) {
+//		for(int j=1;j<=i;j++) {
+//			System.out.print(i+" ");
+//		}
+//		System.out.println();
+//	}
+	
+	
+	
+}
+}
